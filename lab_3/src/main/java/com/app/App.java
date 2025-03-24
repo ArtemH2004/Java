d224a5255e74e889022ce3d126940b1709881aa6
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-03-24 12:35:59 +0300

    lab-3. App

--- lab_3/src/main/java/com/app/App.java
+++ lab_3/src/main/java/com/app/App.java
@@ -1,13 +1,28 @@
 package com.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+
+public class App {
+    
+    public static void main(String[] args) {
+        int operationsCount = 10000;
+
+        List<Integer> arrayList = new ArrayList<>();
+        long arrayListAddTime = ListPerformanceTest.testAdd(arrayList, operationsCount);
+        long arrayListGetTime = ListPerformanceTest.testGet(arrayList, operationsCount);
+        long arrayListRemoveTime = ListPerformanceTest.testRemove(arrayList, operationsCount);
+
+        List<Integer> linkedList = new LinkedList<>();
+        long linkedListAddTime = ListPerformanceTest.testAdd(linkedList, operationsCount);
+        long linkedListGetTime = ListPerformanceTest.testGet(linkedList, operationsCount);
+        long linkedListRemoveTime = ListPerformanceTest.testRemove(linkedList, operationsCount);
+
+        System.out.println("Performance Test Results:");
+        System.out.println("| List Type       | Operations Count | Add Time         | Get Time         | Remove Time      |");
+        System.out.println("|-----------------|------------------|------------------|------------------|------------------|");
+
+        ResultTable.printTable("ArrayList", operationsCount, arrayListAddTime, arrayListGetTime, arrayListRemoveTime);
+
+        ResultTable.printTable("LinkedList", operationsCount, linkedListAddTime, linkedListGetTime, linkedListRemoveTime);
     }
 }
--- lab_3/src/main/java/com/app/ResultTable.java
+++ lab_3/src/main/java/com/app/ResultTable.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class ResultTable {
     public static void printTable(String listType, int operationsCount, long addTime, long getTime, long removeTime) {
-        System.out.printf("| %-15s | %-15d | %-15d | %-15d | %-15d |\n",
+        System.out.printf("| %-15s | %-16d | %-16d | %-16d | %-16d |\n",
                 listType, operationsCount, addTime, getTime, removeTime);
     }
 }
