c2a5db12816aaa6a89efd06c722dac01c37e9285
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-03-26 15:27:47 +0300

    lab-5. Injector

---
+++ lab_5/src/main/java/com/app/core/Injector.java
@@ -0,0 +1,49 @@
+package com.app.core;
+
+import com.app.annotation.AutoInjectable;
+import java.lang.reflect.Field;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class Injector {
+    private Properties properties;
+
+    public Injector() {
+        properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("injector.properties")) {
+            if (input != null) {
+                properties.load(input);
+            } else {
+                throw new RuntimeException("File injector.properties not found in classpath");
+            }
+        } catch (IOException error) {
+            throw new RuntimeException("Error loading settings file", error);
+        }
+    }
+
+    public <T> T inject(T object) {
+        Class<?> clazz = object.getClass();
+        
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(AutoInjectable.class)) {
+                Class<?> fieldType = field.getType();
+                String implementationClassName = properties.getProperty(fieldType.getName());
+                
+                if (implementationClassName == null) {
+                    throw new RuntimeException("No implementation found for interface " + fieldType.getName());
+                }
+                
+                try {
+                    Object implementationInstance = Class.forName(implementationClassName).getDeclaredConstructor().newInstance();
+                    field.setAccessible(true);
+                    field.set(object, implementationInstance);
+                } catch (Exception error) {
+                    throw new RuntimeException("Error injecting dependency", error);
+                }
+            }
+        }
+        
+        return object;
+    }
+}
